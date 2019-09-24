8e6b651dc6caeed1fd426a70acdde091086980f8
Rishabh Karnad <rishabhk@geekyants.com>
2019-09-24 21:15:48 +0530

    Added buttons for navigation

--- screens/Welcome.vue
+++ screens/Welcome.vue
@@ -1,5 +1,34 @@
 <template>
-  <view>
+  <view class="container">
     <text>Welcome</text>
+    <button title="Home" @press="goToHomeScreen"></button>
+    <button title="User" @press="goToUserScreen"></button>
   </view>
 </template>
+
+<script>
+export default {
+  // Declare `navigation` as a prop
+  props: {
+    navigation: {
+      type: Object
+    }
+  },
+  methods: {
+    goToHomeScreen() {
+      this.navigation.navigate("Home")
+    },
+    goToUserScreen() {
+      this.navigation.navigate("User")
+    },
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
